8fb8187092485bc8ea99695cfab38b2347c2e3d3
Murilo Feitosa <muriloalf@gmail.com>
2021-12-02 10:16:05 -0300

    updating

--- src/main/java/org/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/org/generation/blogpessoal/controller/PostagemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,10 +25,12 @@ public class PostagemController {
 		return ResponseEntity.ok(repository.findAll());
 	}
 
-	@GetMapping("/id1")
-	public ResponseEntity<List<PostagemModel>> findById() {
-		return ResponseEntity.ok(repository.findById(1l));
-	}
+	/*@GetMapping("/{id}")
+	public ResponseEntity<List<PostagemModel>> GetById(@PathVariable long id) {
+		return repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}*/
 
 	@GetMapping("/titulo")
 	public ResponseEntity<List<PostagemModel>> GetByTitulo() {
