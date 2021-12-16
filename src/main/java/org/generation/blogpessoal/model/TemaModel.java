aefe2deae716f6728a93e4d5c43e6af02209717d
Murilo Feitosa <muriloalf@gmail.com>
2021-12-16 08:59:19 -0300

    adding security

--- src/main/java/org/generation/blogpessoal/model/TemaModel.java
+++ src/main/java/org/generation/blogpessoal/model/TemaModel.java
@@ -6,6 +6,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -16,7 +17,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @Table(name = "tema")
 public class TemaModel {
 
-	private @NotNull @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
+	private @NotNull @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
 
 	private @NotNull String descricao;
 
